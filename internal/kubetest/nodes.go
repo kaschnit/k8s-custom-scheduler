41e232d27db2b4fc985739fa6a26a206665fa77e
Kevin Schnitzer <kevinschnitzer@gmail.com>
2026-07-01 21:16:08 -0500

    Add basic preemption test

---
+++ internal/kubetest/nodes.go
@@ -0,0 +1,150 @@
+package kubetest
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"slices"
+	"time"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/wait"
+	corev1client "k8s.io/client-go/kubernetes/typed/core/v1"
+	"k8s.io/klog/v2"
+)
+
+func NewKWOKNode(name string) *corev1.Node {
+	return &corev1.Node{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        name,
+			Annotations: map[string]string{"kwok.x-k8s.io/node": "fake"},
+			Labels:      map[string]string{"type": "kwok"},
+		},
+		Spec: corev1.NodeSpec{
+			ProviderID: "kwok://fake-node",
+			Taints:     []corev1.Taint{},
+		},
+	}
+}
+
+type KWOKNodeManager struct {
+	nodeClient corev1client.NodeInterface
+}
+
+func NewKWOKNodeManager(nodeClient corev1client.NodeInterface) *KWOKNodeManager {
+	return &KWOKNodeManager{
+		nodeClient: nodeClient,
+	}
+}
+
+func (mgr *KWOKNodeManager) CreateNodes(ctx context.Context, prefix string, count int) ([]*corev1.Node, error) {
+	var errs error
+	nodes := make([]*corev1.Node, 0, count)
+
+	for i := range count {
+		nodeToCreate := NewKWOKNode(fmt.Sprintf("worker-%d", i))
+
+		createdNode, err := mgr.nodeClient.Create(ctx, nodeToCreate, metav1.CreateOptions{})
+		if err != nil {
+			errs = errors.Join(errs, err)
+		} else {
+			nodes = append(nodes, createdNode)
+		}
+	}
+
+	return nodes, errs
+}
+
+type WaitForNodesReadyOpts struct {
+	PollInterval time.Duration
+	Timeout      time.Duration
+}
+
+func (mgr *KWOKNodeManager) WaitForNodesReady(ctx context.Context, opts WaitForNodesReadyOpts) error {
+	logger := klog.FromContext(ctx)
+
+	if opts.PollInterval == 0 {
+		opts.PollInterval = time.Second
+	}
+	if opts.Timeout == 0 {
+		opts.Timeout = time.Minute
+	}
+
+	return wait.PollUntilContextTimeout(ctx, opts.PollInterval, opts.Timeout, true,
+		func(ctx context.Context) (done bool, err error) {
+			nodes, err := mgr.nodeClient.List(ctx, metav1.ListOptions{LabelSelector: "type=kwok"})
+			if err != nil {
+				return false, err
+			}
+
+			readyCount := 0
+			for _, node := range nodes.Items {
+				for _, cond := range node.Status.Conditions {
+					if cond.Type == corev1.NodeReady && cond.Status == corev1.ConditionTrue {
+						updatedTaints := slices.DeleteFunc(slices.Clone(node.Spec.Taints),
+							func(tnt corev1.Taint) bool {
+								return tnt.Key == "node.kubernetes.io/not-ready"
+							})
+
+						// Remove not-ready taint if it was present.
+						if len(updatedTaints) < len(node.Spec.Taints) {
+							patch, err := json.Marshal(map[string]any{
+								"spec": map[string]any{
+									"taints": updatedTaints,
+								},
+							})
+							if err != nil {
+								return false, err
+							}
+
+							_, err = mgr.nodeClient.Patch(
+								ctx,
+								node.Name,
+								types.MergePatchType,
+								patch,
+								metav1.PatchOptions{},
+							)
+							if err != nil {
+								return false, err
+							}
+						}
+
+						readyCount++
+						break
+					}
+				}
+			}
+
+			logger.V(5).Info("Checked nodes ready",
+				"ready", readyCount,
+				"total", len(nodes.Items))
+
+			return readyCount == len(nodes.Items), nil
+		})
+}
+
+func (mgr *KWOKNodeManager) CreateNodesAndWaitForReady(
+	ctx context.Context,
+	prefix string,
+	count int,
+	opts WaitForNodesReadyOpts,
+) error {
+
+	if _, err := mgr.CreateNodes(ctx, prefix, count); err != nil {
+		return err
+	}
+
+	if err := mgr.WaitForNodesReady(ctx, opts); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (mgr *KWOKNodeManager) DeleteAllNodes(ctx context.Context) error {
+	return mgr.nodeClient.DeleteCollection(ctx, *metav1.NewDeleteOptions(0),
+		metav1.ListOptions{LabelSelector: "type=kwok"})
+}
--- internal/plugin/quotaawarepreempt/plugin_envtest_test.go
+++ internal/plugin/quotaawarepreempt/plugin_envtest_test.go
@@ -3,22 +3,23 @@
 package quotaawarepreempt_test
 
 import (
-	"context"
-	"encoding/json"
-	"fmt"
-	"slices"
 	"testing"
 	"time"
 
+	schedulingapi "github.com/kaschnit/kaschnit-scheduler/apis/scheduling"
+	schedulingv1 "github.com/kaschnit/kaschnit-scheduler/apis/scheduling/v1"
+	schedulingclient "github.com/kaschnit/kaschnit-scheduler/client/clientset/scheduling"
 	"github.com/kaschnit/kaschnit-scheduler/internal/kubesched"
+	"github.com/kaschnit/kaschnit-scheduler/internal/kubetest"
 	"github.com/kaschnit/kaschnit-scheduler/internal/plugin/quotaawarepreempt"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	corev1 "k8s.io/api/core/v1"
+	kubeschedulingv1 "k8s.io/api/scheduling/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/dynamic/dynamicinformer"
 	"k8s.io/client-go/informers"
@@ -26,10 +27,10 @@ import (
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/restmapper"
 	"k8s.io/client-go/tools/events"
-	schedulerconfigv1 "k8s.io/kube-scheduler/config/v1"
+	kubeschedcfgv1 "k8s.io/kube-scheduler/config/v1"
 	"k8s.io/kubernetes/pkg/scheduler"
-	schedulerconfigapi "k8s.io/kubernetes/pkg/scheduler/apis/config"
-	schedulerq "k8s.io/kubernetes/pkg/scheduler/backend/queue"
+	kubeschedcfgapi "k8s.io/kubernetes/pkg/scheduler/apis/config"
+	kubeschedq "k8s.io/kubernetes/pkg/scheduler/backend/queue"
 	"k8s.io/kubernetes/pkg/scheduler/framework/runtime"
 	"k8s.io/utils/clock"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
@@ -70,6 +71,9 @@ func TestScheduler(t *testing.T) {
 	dynClient, err := dynamic.NewForConfig(k8sConfig)
 	require.NoError(t, err, "Failed to create dynamic Kubernetes client")
 
+	schedulingClient, err := schedulingclient.NewForConfig(k8sConfig)
+	require.NoError(t, err, "Failed to create scheduling client")
+
 	kwokClient, err := kwokclient.NewForConfig(k8sConfig)
 	require.NoError(t, err, "Failed to create KWOK clientset")
 
@@ -82,7 +86,10 @@ func TestScheduler(t *testing.T) {
 	require.NoError(t, err, "Failed to unmarshal default node init stage")
 
 	podInitStage, err := config.UnmarshalWithType[*kwokinternal.Stage](podfast.DefaultPodReady)
-	require.NoError(t, err, "Failed to unmarshal default pod read stage")
+	require.NoError(t, err, "Failed to unmarshal default pod ready stage")
+
+	podDeleteStage, err := config.UnmarshalWithType[*kwokinternal.Stage](podfast.DefaultPodDelete)
+	require.NoError(t, err, "Failed to unmarshal default pod delete stage")
 
 	kwokCtrl, err := kwokctrl.NewController(kwokctrl.Config{
 		TypedClient:                       k8sClient,
@@ -100,7 +107,7 @@ func TestScheduler(t *testing.T) {
 		Clock:                             clck,
 		LocalStages: map[kwokinternal.StageResourceRef][]*kwokinternal.Stage{
 			{APIGroup: "v1", Kind: "Node"}: {nodeInitStage},
-			{APIGroup: "v1", Kind: "Pod"}:  {podInitStage},
+			{APIGroup: "v1", Kind: "Pod"}:  {podInitStage, podDeleteStage},
 		},
 	})
 	require.NoError(t, err, "Failed to create KWOK controller")
@@ -108,126 +115,139 @@ func TestScheduler(t *testing.T) {
 	err = kwokCtrl.Start(t.Context())
 	require.NoError(t, err, "Failed to start KWOK controller")
 
-	t.Logf("Creating %d nodes", numNodes)
-	for i := range numNodes {
-		node := &corev1.Node{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:        fmt.Sprintf("worker-%d", i),
-				Annotations: map[string]string{"kwok.x-k8s.io/node": "fake"},
-				Labels:      map[string]string{"type": "kwok"},
-			},
-			Spec: corev1.NodeSpec{
-				ProviderID: "kwok://fake-node",
-				Taints:     []corev1.Taint{},
-			},
-		}
+	nodeMgr := kubetest.NewKWOKNodeManager(k8sClient.CoreV1().Nodes())
 
-		_, err := k8sClient.CoreV1().Nodes().Create(t.Context(), node, metav1.CreateOptions{})
-		require.NoError(t, err, "Failed to create worker node %d", i, numNodes)
+	registerResourceCleanup := func(t *testing.T) {
+		t.Helper()
+		t.Cleanup(func() { nodeMgr.DeleteAllNodes(t.Context()) })
+		t.Cleanup(func() {
+			k8sClient.CoreV1().Pods(corev1.NamespaceAll).DeleteCollection(t.Context(),
+				*metav1.NewDeleteOptions(0), metav1.ListOptions{})
+		})
 	}
 
-	t.Logf("Waiting for %d nodes to be ready", numNodes)
-	// TODO switch to eventual assertion.
-	err = wait.PollUntilContextTimeout(t.Context(), 1*time.Second, 10*time.Second, true,
-		func(ctx context.Context) (done bool, err error) {
-			nodes, err := k8sClient.CoreV1().Nodes().List(ctx, metav1.ListOptions{})
-			if err != nil {
-				t.Logf("Failed to list nodes: %s", err)
-				return false, err
-			}
+	priorityClasses := []*kubeschedulingv1.PriorityClass{
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "high",
+			},
+			Value:            1000,
+			PreemptionPolicy: new(corev1.PreemptLowerPriority),
+		},
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "low",
+			},
+			Value:            -1000,
+			PreemptionPolicy: new(corev1.PreemptLowerPriority),
+		},
+	}
+	for _, pc := range priorityClasses {
+		_, err := k8sClient.SchedulingV1().PriorityClasses().Create(t.Context(), pc, metav1.CreateOptions{})
+		require.NoError(t, err, "Failed to create priority class")
+	}
 
-			if len(nodes.Items) < numNodes {
-				return false, nil
-			}
+	t.Run("Basic quota preemption", func(t *testing.T) {
+		registerResourceCleanup(t)
 
-			readyCount := 0
-			for _, node := range nodes.Items {
-				for _, cond := range node.Status.Conditions {
-					if cond.Type == corev1.NodeReady && cond.Status == corev1.ConditionTrue {
-						readyCount++
+		err = nodeMgr.CreateNodesAndWaitForReady(t.Context(), "worker", numNodes,
+			kubetest.WaitForNodesReadyOpts{
+				PollInterval: time.Second,
+				Timeout:      10 * time.Second,
+			})
+		require.NoError(t, err, "Failed to create nodes and wait for ready")
 
-						if len(node.Spec.Taints) > 0 {
-							updatedTaints := slices.DeleteFunc(slices.Clone(node.Spec.Taints),
-								func(tnt corev1.Taint) bool { return tnt.Key == "node.kubernetes.io/not-ready" })
-							if len(updatedTaints) < len(node.Spec.Taints) {
-								patch, err := json.Marshal(map[string]any{
-									"spec": map[string]any{
-										"taints": updatedTaints,
-									},
-								})
-								require.NoError(t, err, "Failed to marshal node taint patch")
-
-								_, err = k8sClient.CoreV1().Nodes().Patch(
-									t.Context(),
-									node.Name,
-									types.MergePatchType,
-									patch,
-									metav1.PatchOptions{},
-								)
-								if err != nil {
-									t.Logf("Failed to remove taint from node: %s", err)
-									return false, err
-								}
-							}
-						}
-
-						break
-					}
-				}
-			}
-
-			t.Logf("Progress: %d/%d nodes are Ready", readyCount, numNodes)
-			return readyCount == numNodes, nil
-		})
-	require.NoError(t, err, "Nodes never became ready")
-
-	t.Run("schedule one pod", func(t *testing.T) {
 		kubeScheduler := newKubeScheduler(t, k8sConfig, k8sClient, dynClient)
 
-		pod := &corev1.Pod{
+		_, err := schedulingClient.SchedulingV1().Queues().Create(t.Context(), &schedulingv1.Queue{
 			TypeMeta: metav1.TypeMeta{
-				APIVersion: "v1",
-				Kind:       "Pod",
+				APIVersion: schedulingv1.SchemeGroupVersion.String(),
+				Kind:       "Queue",
 			},
 			ObjectMeta: metav1.ObjectMeta{
-				Name: "pod-1",
+				Name: "tenant-a",
 			},
-			Spec: corev1.PodSpec{
-				SchedulerName: schedulerName,
-				Containers: []corev1.Container{
-					{
-						Name:            "test-container",
-						Image:           "fake-image",
-						ImagePullPolicy: corev1.PullIfNotPresent,
-						Resources: corev1.ResourceRequirements{
-							Requests: corev1.ResourceList{
-								corev1.ResourceCPU:    resource.MustParse("2"),
-								corev1.ResourceMemory: resource.MustParse("1Gi"),
-							},
-						},
+			Spec: schedulingv1.QueueSpec{
+				Quota: schedulingv1.QuotaSpec{
+					Max: corev1.ResourceList{
+						corev1.ResourceCPU:    resource.MustParse("3"),
+						corev1.ResourceMemory: resource.MustParse("5Gi"),
+					},
+				},
+				Preemption: schedulingv1.PreemptionSpec{
+					Preempts: schedulingv1.PreemptsRule{
+						FromPods: &metav1.LabelSelector{},
+						ToQueues: &metav1.LabelSelector{},
+						ToPods:   &metav1.LabelSelector{},
+					},
+					PreemptedBy: schedulingv1.PreemptedByRule{
+						FromQueues: &metav1.LabelSelector{},
+						FromPods:   &metav1.LabelSelector{},
+						ToPods:     &metav1.LabelSelector{},
 					},
 				},
 			},
-		}
+		}, metav1.CreateOptions{})
+		require.NoError(t, err, "Failed to create queue")
 
-		_, err = k8sClient.CoreV1().Pods(corev1.NamespaceDefault).Create(t.Context(), pod, metav1.CreateOptions{})
-		require.NoError(t, err, "Failed to create pod")
+		victim := newPodForQueue(types.NamespacedName{Name: "victim-1"}, "tenant-a", "low",
+			corev1.ResourceList{
+				corev1.ResourceCPU:    resource.MustParse("3"),
+				corev1.ResourceMemory: resource.MustParse("1Gi"),
+			})
+		_, err = k8sClient.CoreV1().Pods(corev1.NamespaceDefault).Create(t.Context(),
+			victim, metav1.CreateOptions{})
+		require.NoError(t, err)
 
-		podList, err := k8sClient.CoreV1().Pods(corev1.NamespaceDefault).List(t.Context(), metav1.ListOptions{})
-		require.NoError(t, err, "Failed to list pods before scheduling")
-		assert.Len(t, podList.Items, 1, "Pod list should have 1 item")
-		assert.Empty(t, podList.Items[0].Spec.NodeName, "Pod should not have spec.nodeName before scheduling")
-		assert.Equal(t, corev1.PodPending, podList.Items[0].Status.Phase, "Pod should be pending before scheduling")
-
+		// Schedule victim pod
 		kubeScheduler.ScheduleOne(t.Context())
+		require.EventuallyWithT(t, func(c *assert.CollectT) {
+			victim, err := k8sClient.CoreV1().Pods(corev1.NamespaceDefault).Get(t.Context(),
+				victim.Name, metav1.GetOptions{})
+			require.NoError(c, err, "Failed to get victim pod")
+			assert.Equal(c, corev1.PodRunning, victim.Status.Phase, "Victim should be running")
+			assert.NotEmpty(c, victim.Spec.NodeName, "Victim should have nodeName")
+		}, 3*time.Second, 1*time.Second)
 
-		time.Sleep(3 * time.Second) // TODO use eventual assertion
+		victim, err = k8sClient.CoreV1().Pods(corev1.NamespaceDefault).Get(t.Context(),
+			victim.Name, metav1.GetOptions{})
+		require.NoError(t, err, "Failed to get victim pod")
 
-		podList, err = k8sClient.CoreV1().Pods(corev1.NamespaceDefault).List(t.Context(), metav1.ListOptions{})
-		require.NoError(t, err, "Failed to list pods after scheduling")
-		assert.Len(t, podList.Items, 1, "Pod list should have 1 item")
-		assert.NotEmpty(t, podList.Items[0].Spec.NodeName, "Pod should have spec.nodeName after scheduling")
-		assert.NotEqual(t, corev1.PodPending, podList.Items[0].Status.Phase, "Pod should not be pending before scheduling")
+		preemptor := newPodForQueue(types.NamespacedName{Name: "preemptor-1"}, "tenant-a", "high",
+			corev1.ResourceList{
+				corev1.ResourceCPU:    resource.MustParse("2"),
+				corev1.ResourceMemory: resource.MustParse("1Gi"),
+			})
+		_, err = k8sClient.CoreV1().Pods(corev1.NamespaceDefault).Create(t.Context(),
+			preemptor, metav1.CreateOptions{})
+		require.NoError(t, err)
+
+		// Perform preemption, resulting in nominated node for preemptor pod
+		kubeScheduler.ScheduleOne(t.Context())
+		require.EventuallyWithT(t, func(c *assert.CollectT) {
+			preemptor, err := k8sClient.CoreV1().Pods(corev1.NamespaceDefault).Get(t.Context(),
+				preemptor.Name, metav1.GetOptions{})
+			require.NoError(c, err, "Failed to get preemptor pod")
+			assert.NotEmpty(c, preemptor.Status.NominatedNodeName, "Preemptor should have nominatedNode")
+			assert.Equal(c, victim.Spec.NodeName, preemptor.Status.NominatedNodeName,
+				"Preemptor's nominated node should be the victim's node")
+
+			_, err = k8sClient.CoreV1().Pods(corev1.NamespaceDefault).Get(t.Context(),
+				victim.Name, metav1.GetOptions{})
+			assert.True(c, apierrors.IsNotFound(err), "Victim pod should be deleted")
+		}, 3*time.Second, 1*time.Second)
+
+		// Perform scheduling for nominated node
+		kubeScheduler.ScheduleOne(t.Context())
+		require.EventuallyWithT(t, func(c *assert.CollectT) {
+			preemptor, err := k8sClient.CoreV1().Pods(corev1.NamespaceDefault).Get(t.Context(),
+				preemptor.Name, metav1.GetOptions{})
+			require.NoError(c, err, "Failed to get preemptor pod")
+			assert.Equal(c, corev1.PodRunning, preemptor.Status.Phase, "Preemptor should be running after scheduling")
+			assert.NotEmpty(c, preemptor.Spec.NodeName, "Preemptor should have spec.nodeName after scheduling")
+			assert.Equal(c, victim.Spec.NodeName, preemptor.Spec.NodeName,
+				"Preemptor's node should be the victim's node")
+		}, 3*time.Second, 1*time.Second)
 	})
 }
 
@@ -266,7 +286,7 @@ func newKubeScheduler(
 		scheduler.WithPercentageOfNodesToScore(kubeSchedulerConfig.PercentageOfNodesToScore),
 		scheduler.WithPodMaxBackoffSeconds(kubeSchedulerConfig.PodMaxBackoffSeconds),
 		scheduler.WithPodInitialBackoffSeconds(kubeSchedulerConfig.PodInitialBackoffSeconds),
-		scheduler.WithPodMaxInUnschedulablePodsDuration(schedulerq.DefaultPodMaxInUnschedulablePodsDuration),
+		scheduler.WithPodMaxInUnschedulablePodsDuration(kubeschedq.DefaultPodMaxInUnschedulablePodsDuration),
 		scheduler.WithParallelism(kubeSchedulerConfig.Parallelism),
 	)
 	require.NoError(t, err, "Failed to create scheduler instance")
@@ -274,27 +294,27 @@ func newKubeScheduler(
 	return kubeScheduler
 }
 
-func newKubeSchedulerConfig(t *testing.T) schedulerconfigapi.KubeSchedulerConfiguration {
+func newKubeSchedulerConfig(t *testing.T) kubeschedcfgapi.KubeSchedulerConfiguration {
 	t.Log("Building scheduler profile")
-	kubeSchedulerConfig, err := kubesched.ToConfigAPIWithDefaults(schedulerconfigv1.KubeSchedulerConfiguration{
+	kubeSchedulerConfig, err := kubesched.ToConfigAPIWithDefaults(kubeschedcfgv1.KubeSchedulerConfiguration{
 		TypeMeta: metav1.TypeMeta{
-			APIVersion: schedulerconfigv1.SchemeGroupVersion.String(),
+			APIVersion: kubeschedcfgv1.SchemeGroupVersion.String(),
 			Kind:       "KubeSchedulerConfiguration",
 		},
-		Profiles: []schedulerconfigv1.KubeSchedulerProfile{
+		Profiles: []kubeschedcfgv1.KubeSchedulerProfile{
 			{
 				SchedulerName: new(schedulerName),
-				Plugins: &schedulerconfigv1.Plugins{
-					MultiPoint: schedulerconfigv1.PluginSet{
-						Enabled: []schedulerconfigv1.Plugin{
+				Plugins: &kubeschedcfgv1.Plugins{
+					MultiPoint: kubeschedcfgv1.PluginSet{
+						Enabled: []kubeschedcfgv1.Plugin{
 							{Name: quotaawarepreempt.PluginName},
 						},
 					},
-					PostFilter: schedulerconfigv1.PluginSet{
-						Enabled: []schedulerconfigv1.Plugin{
+					PostFilter: kubeschedcfgv1.PluginSet{
+						Enabled: []kubeschedcfgv1.Plugin{
 							{Name: quotaawarepreempt.PluginName},
 						},
-						Disabled: []schedulerconfigv1.Plugin{
+						Disabled: []kubeschedcfgv1.Plugin{
 							{Name: "*"},
 						},
 					},
@@ -315,4 +335,47 @@ func newPluginRegistry(t *testing.T) runtime.Registry {
 	require.NoError(t, err, "Failed to registery scheduler plugin")
 
 	return pluginRegistry
+}
+
+func newPodForQueue(
+	name types.NamespacedName,
+	queue string,
+	priorityClassName string,
+	requests corev1.ResourceList,
+) *corev1.Pod {
+	pod := newPod(name, requests)
+	pod.Labels[schedulingapi.LabelKeyQueue] = queue
+	pod.Spec.PriorityClassName = priorityClassName
+	return pod
+}
+
+func newPod(name types.NamespacedName, requests corev1.ResourceList) *corev1.Pod {
+	if requests == nil {
+		requests = corev1.ResourceList{
+			corev1.ResourceCPU:    resource.MustParse("1"),
+			corev1.ResourceMemory: resource.MustParse("1Gi"),
+		}
+	}
+
+	return &corev1.Pod{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "v1",
+			Kind:       "Pod",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name.Name,
+			Namespace: name.Namespace,
+			Labels:    make(map[string]string),
+		},
+		Spec: corev1.PodSpec{
+			SchedulerName: schedulerName,
+			Containers: []corev1.Container{
+				{
+					Name:      "test-container",
+					Image:     "fake-image",
+					Resources: corev1.ResourceRequirements{Requests: requests},
+				},
+			},
+		},
+	}
 }
